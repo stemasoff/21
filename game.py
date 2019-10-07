6d5167f22a7e52a2eab8443e014e58a16c3f1606
Ilya <stemasoff1@yandex.ru>
2019-10-07 14:42:50 +0300

    commit changes

--- game.py
+++ game.py
@@ -1,26 +1,37 @@
 import objects
 from random import shuffle
 
-def check_score(player_score, dealer_score, end = False):
-    if (player_score > dealer_score and end == True) \
+
+def check_score(player_score, dealer_score, end=False):
+    '''
+    Функция проверяет количество очков у игрока и дилера.
+    :param player_score: количество очков у игрока.
+    :param dealer_score: количество очков у дилера.
+    :param end: параметр, отвечающий за конец игры. Если True - игра закончена.
+    :return: ход или итог игры.
+    '''
+    if (player_score > dealer_score and end is True) \
             or dealer_score > 21 or player_score == 21:
         return 'Ты выиграл.'
-    elif end == False:
-        return 'Игра продолжается'
-    else:
+    elif player_score > 21 or player_score < dealer_score and end is True:
         return 'Ты проиграл.'
-
-deck = objects.Deck()
-player = objects.Player()
-dealer = objects.Dealer()
-action = ''
+    else:
+        return 'Идёт игра'
 
 
-shuffle(deck.deck)
+deck = objects.Deck()  # Колода
+player = objects.Player()   # Игрок
+dealer = objects.Dealer()   # Дилер
+action = ''     # Действие игрока
+
+shuffle(deck.deck)  # Перемешиваем колоду
+# Ход игры
 game_progress = 'Игра началась, колода помешана. В игре 2 игрока, Dealer и Player'
 print(game_progress)
+game_progress = 'Идёт игра'
 
-while action != '2' or game_progress == 'Игра продолжается':
+# Цикл взаимодействия с пользователем в соответствии с правилами игры.
+while game_progress == 'Идёт игра':
     print('1 - взять еще\n2 - закончить')
     action = input('Ваше действие: ')
     if action == '1':
@@ -29,7 +40,6 @@ while action != '2' or game_progress == 'Игра продолжается':
         print(player)
         game_progress = check_score(player.score, dealer.score)
     elif action == '2':
-        game_progress = check_score(player.score, dealer.score, end = True)
+        game_progress = check_score(player.score, dealer.score, end=True)
     print(game_progress)
-print(game_progress)
 print('Ваш счет: {}\nСчет dealer: {}'.format(player.score, dealer.score))
--- objects.py
+++ objects.py
@@ -1,10 +1,21 @@
+"""
+Модуль objects.py содержит в себе классы, которые будут
+взаимодействовать в ходе игры.
+"""
+
 class Cards:
+    """
+    Класс карты имеет два параметра, значение и масть.
+    """
     def __init__(self, value=None, suit=None):
         self.value = value
         self.suit = suit
     def __str__(self):
         return '{} {}'.format(self.value, self.suit)
 class Deck:
+    """
+    Колода содержит в себе 36 объектов класса Карта.
+    """
     value = [i for i in range(6, 11)]
     value.append('J')
     value.append('Q')
@@ -16,14 +27,22 @@ class Deck:
         for j in suit:
             deck.append(Cards(i, j))
 
-    def __repr__(self):
+    def __str__(self):
         return 'В колоде {} карт.'.format(len(self.deck))
 
 class Player:
+    """
+    Класс player имеет 2 параметра, это карта, и его счет.
+    Player может взять карту и посчитать, сколько у него очков.
+    """
     def __init__(self):
         self.cards = []
         self.score = int()
     def add_cards(self, card):
+        '''
+        Функция принимает карту и считает, сколько очков у player.
+        :param card: экземпляр класса Cards
+        '''
         self.cards.append(card)
         if isinstance(self.cards[-1].value, int):
             self.score = self.score + self.cards[-1].value
@@ -36,9 +55,14 @@ class Player:
         else:
             self.score = self.score + 11
 
-    def __repr__(self):
+    def __str__(self):
         return 'Ваш счет: {}\nВаша карта: {}'.format(self.score, self.cards[-1])
+
 class Dealer(Player, Deck):
+    """
+    Класс dealer наследует все свойства от класса player и deck.
+    Dealer может снимать с колоды последнюю карту.
+    """
     def more(self):
         return self.deck.pop()
 
